ec147821c318f7d850034687ad50710b0097da5a
Peipei Wong <wangpp1995@gmail.com>
2018-07-30 16:02:45 +0800

    Add message box bottom

--- src/components/messageBox/messageBox.js
+++ src/components/messageBox/messageBox.js
@@ -1,24 +1,28 @@
 Component({
-	data: {
-		message: '',
-		enableSendMessage: false
-	},
-	methods: {
-		sendMessage: function () {
-			console.log(this);
-			console.log(this.data.message);
-		},
+  data: {
+    message: '',
+    chat: [],
+    recommendWord: ['语音助手可以做什么', '它有哪些应用案例', '同类公司', '尝试发送推荐'],
+    enableSendMessage: false,
+  },
+  methods: {
+    sendMessage: function () {
+      console.log(this.data.message);
+    },
 
-		handleInput: function (e) {
-			var that = this
-			this.setData({
-				message: e.detail.value,
-				enableSendMessage: that.checkIfEnableSendMessage()
-			})
-		},
+    handleInput: function (e) {
+      this.setData({
+        message: e.detail.value,
+        enableSendMessage: this.checkIfEnableSendMessage()
+      });
+    },
 
-		checkIfEnableSendMessage: function() {
-			return !!(this.data.message.length > 0)
-		}
-	},
+    checkIfEnableSendMessage: function () {
+      return !!(this.data.message.length > 0);
+    },
+
+    sendRecommendWord: function () {
+
+    },
+  },
 });
